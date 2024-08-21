d5c203bdc85a5cddc19a833c3dce1ff5340cfb9b
Hiroshinoharu <maxceban2019@gmail.com>
2024-08-21 20:13:41 +0100

    Set up JavaFx with comments

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,10 +10,16 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+			// BorderPane is a layout manager that manages all the nodes in 5 areas:
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
+			
+			// Add a CSS file to the scene
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
+			// Set the title of the window
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("Tic Tac Toe Game");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
